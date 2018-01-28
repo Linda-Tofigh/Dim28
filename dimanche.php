aba6bc330f9cb9402703728640d873f5be24bfba
Linda Tofigh <linda_tofigh@yahoo.com>
2018-01-28 17:54:12 +0100

    Premiere commit(addition)

---
+++ dimanche.php
@@ -0,0 +1,16 @@
+<?php
+$nombre1 = $_POST['nombre1'];
+$nombre2 = $_POST['nombre2'];
+$function = $_POST['function'];
+
+if(isset($_POST['nombre1'])&&isset($_POST['nombre2'])) {
+
+    if ($function == '+') {
+        $nombre3 = $nombre1 + $nombre2;
+        echo $nombre1 . " + " . $nombre2 . " = " . $nombre3;
+    }
+
+}
+
+
+?>
