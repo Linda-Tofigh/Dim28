c7e6873686aa9a21a479e914eb53242efa43a21c
Linda Tofigh <linda_tofigh@yahoo.com>
2018-01-28 19:28:25 +0100

    Cinquième commit(Ajouter num1 et num2 )

--- dimanche.php
+++ dimanche.php
@@ -27,6 +27,19 @@ if(isset($_POST['nombre1'])&&isset($_POST['nombre2'])) {
     }
 
 }
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Calculatrice</title>
+</head>
+<body>
+<form action="dimanche.php" method="POST" style="text-align: center; margin-top: 100px; ">
+    num1: <input type="text" name="nombre1" placeholder="Entrer premier nombre "><br >
+    num2: <input type="text" name="nombre2" placeholder="Entrer deuxième nombre" style="margin-top: 20px"><br >
+
+</form>
+</body>
+</html>
