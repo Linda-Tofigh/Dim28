68757f26ea60e7f2e834c941b4b67f5432925040
Linda Tofigh <linda_tofigh@yahoo.com>
2018-01-28 19:10:44 +0100

    Troisième commit(Multiplication)

--- dimanche.php
+++ dimanche.php
@@ -14,6 +14,12 @@ if(isset($_POST['nombre1'])&&isset($_POST['nombre2'])) {
         $nombre3 = $nombre1 - $nombre2;
         echo $nombre1 ." - ".$nombre2." = ".$nombre3;
     }
+
+    if($function =='*')
+    {
+        $nombre3 = $nombre1 * $nombre2;
+        echo $nombre1 ." * ".$nombre2." = ".$nombre3;
+    }
 }
 
 
